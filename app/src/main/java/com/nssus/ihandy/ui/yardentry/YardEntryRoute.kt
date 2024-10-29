dc24ce2b13911466cddd32b756a29feb6d9b1c4a
Intat Likitpolchaloon <intat@orcsoft.co.th>
2024-10-29 14:30:48 +0700

    implement button dialog, convert call vm to send action

--- app/src/main/java/com/nssus/ihandy/ui/yardentry/YardEntryRoute.kt
+++ app/src/main/java/com/nssus/ihandy/ui/yardentry/YardEntryRoute.kt
@@ -4,7 +4,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.navigation.NavController
+import com.nssus.ihandy.model.yardentry.YardEntryAction
 import com.nssus.ihandy.model.yardentry.YardEntryNavigateType
+import com.nssus.ihandy.ui.basecomposable.BaseTitleMsgWithButtonDialog
 import com.nssus.ihandy.ui.basecomposable.CustomLoading
 import com.nssus.ihandy.ui.yardentry.viewmodel.YardEntryViewModel
 
@@ -25,14 +27,21 @@ fun YardEntryRoute(
             when (uiYardEntrySt.navigateType) {
                 YardEntryNavigateType.GO_BACK -> {
                     navController.popBackStack()
-                    yardEntryVm.initNavigateData()
+//                    yardEntryVm.initNavigateData()
+                    yardEntryVm.action(YardEntryAction.InitNavigateData) //
+                }
+                YardEntryNavigateType.DISPLAY_BUTTON_DIALOG -> {
+                    BaseTitleMsgWithButtonDialog(
+                        description = uiYardEntrySt.successMsg,
+                        onCloseDialog = { yardEntryVm.action(YardEntryAction.InitNavigateData) },
+                        onLeftButtonClick = { yardEntryVm.action(YardEntryAction.ClickContinueDialogButton) },
+                        onRightButtonClick = { yardEntryVm.action(YardEntryAction.ClearAllValueButton) }
+                    )
                 }
                 else -> Unit
             }
         }
-        uiYardEntrySt.isError -> {
-
-        }
+        uiYardEntrySt.isError -> {}
     }
 
     YardEntryScreen(
