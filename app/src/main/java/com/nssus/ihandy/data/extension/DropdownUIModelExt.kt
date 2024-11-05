8b9530841351a013d3e4af18c47e3a131ad46f33
Intat Likitpolchaloon <intat@orcsoft.co.th>
2024-11-05 10:52:17 +0700

    call selectedItem instead

--- app/src/main/java/com/nssus/ihandy/data/extension/DropdownUIModelExt.kt
+++ app/src/main/java/com/nssus/ihandy/data/extension/DropdownUIModelExt.kt
@@ -5,7 +5,7 @@ import com.nssus.ihandy.model.ui.DropdownUIModel
 fun List<DropdownUIModel>.setSelectItemFrom(selectedItem: DropdownUIModel) {
     apply {
         // ปรับเป็น ถ้าเจอตัวเดิมไม่ต้องเข้าก็ได้
-        find { it.isSelected }?.let { it.isSelected = false }
+        getSelectedItem()?.let { it.isSelected = false }
         find { selectedItem.value == it.value }?.let { it.isSelected = true }
     }
 }
