53ba241e29433ab1e91dfbefd8fc61f914149613
Intat Likitpolchaloon <intat@orcsoft.co.th>
2024-11-05 10:55:09 +0700

    add constant

--- app/src/main/java/com/nssus/ihandy/ui/yardentry/constant/YardEntryConstant.kt
+++ app/src/main/java/com/nssus/ihandy/ui/yardentry/constant/YardEntryConstant.kt
@@ -6,4 +6,7 @@ object YardEntryConstant {
     const val MAX_LENGTH_YYRRCCT = 6
     const val MAX_LENGTH_SUPPLIER_NO = 12
 
+    const val TIME_TO_GET_TRUCK_NO = 3L // Second
+    const val TIME_TO_GET_COIL_NO = 5L // Second
+
 }
