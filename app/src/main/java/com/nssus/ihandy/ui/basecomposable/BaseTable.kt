bd9859a7e29213573685cf56757d02ee64a8ee19
Intat Likitpolchaloon <intat@orcsoft.co.th>
2024-11-06 13:39:38 +0700

    create table header and detail row (2 and 3 column)

---
+++ app/src/main/java/com/nssus/ihandy/ui/basecomposable/BaseTable.kt
@@ -0,0 +1,223 @@
+package com.nssus.ihandy.ui.basecomposable
+
+import androidx.annotation.StringRes
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.nssus.ihandy.R
+import com.nssus.ihandy.ui.theme.BaseGreen
+import com.nssus.ihandy.ui.theme.FontStyles
+import com.nssus.ihandy.ui.theme.StatusRed
+import com.nssus.ihandy.ui.theme.White87
+
+@Composable
+fun Table3ColumnHeaderRow(
+    firstColumnWeight: Float = .5f,
+    secondColumnWeight: Float = .25f,
+    thirdColumnWeight: Float = .25f,
+    @StringRes firstTileId: Int = R.string.common_coil_no_table_title,
+    @StringRes secondTileId: Int = R.string.common_status_table_title,
+    @StringRes thirdTileId: Int = R.string.common_dock_table_title
+) {
+    Row(modifier = Modifier.fillMaxWidth()) {
+        Text(
+            modifier = Modifier.weight(firstColumnWeight),
+            text = stringResource(id = firstTileId),
+            style = FontStyles.txt20,
+            fontWeight = FontWeight.Medium,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            modifier = Modifier.weight(secondColumnWeight),
+            text = stringResource(id = secondTileId),
+            style = FontStyles.txt20,
+            fontWeight = FontWeight.Medium,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            modifier = Modifier.weight(thirdColumnWeight),
+            text = stringResource(id = thirdTileId),
+            style = FontStyles.txt20,
+            fontWeight = FontWeight.Medium,
+            textAlign = TextAlign.Center
+        )
+    }
+}
+
+@Composable
+fun Table3ColumnDetailRow(
+    firstColumnWeight: Float = .5f,
+    secondColumnWeight: Float = .25f,
+    thirdColumnWeight: Float = .25f,
+    isFirstColumnValueMatched: Boolean,
+    firstColumnValue: String,
+    secondColumnValue: String,
+    thirdColumnValue: String,
+    rowBottomSpace: Dp = 10.dp
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(IntrinsicSize.Min)
+    ) {
+        Text(
+            modifier = Modifier.weight(firstColumnWeight),
+            text = firstColumnValue,
+            style = FontStyles.txt16,
+            fontWeight = FontWeight.Medium,
+            color = if (isFirstColumnValueMatched) BaseGreen else Color.Black
+        )
+        Spacer(modifier = Modifier.width(10.dp))
+        Text(
+            modifier = Modifier
+                .background(White87)
+                .padding(horizontal = 4.dp)
+                .fillMaxHeight()
+                .weight(secondColumnWeight),
+            text = secondColumnValue,
+            style = FontStyles.txt16,
+            fontWeight = FontWeight.Bold,
+            color = StatusRed,
+            textAlign = TextAlign.Center
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Text(
+            modifier = Modifier
+                .background(White87)
+                .padding(horizontal = 4.dp)
+                .fillMaxHeight()
+                .weight(thirdColumnWeight),
+            text = thirdColumnValue,
+            style = FontStyles.txt16,
+            fontWeight = FontWeight.Bold,
+            color = StatusRed,
+            textAlign = TextAlign.Center
+        )
+    }
+    Spacer(modifier = Modifier.height(rowBottomSpace))
+}
+
+@Composable
+fun Table2ColumnHeaderRow(
+    firstColumnWeight: Float = .7f,
+    secondColumnWeight: Float = .3f,
+    @StringRes firstTileId: Int = R.string.common_coil_no_table_title,
+    @StringRes secondTileId: Int = R.string.common_status_table_title
+) {
+    Row(modifier = Modifier.fillMaxWidth()) {
+        Text(
+            modifier = Modifier.weight(firstColumnWeight),
+            text = stringResource(id = firstTileId),
+            style = FontStyles.txt20,
+            fontWeight = FontWeight.Medium,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            modifier = Modifier.weight(secondColumnWeight),
+            text = stringResource(id = secondTileId),
+            style = FontStyles.txt20,
+            fontWeight = FontWeight.Medium,
+            textAlign = TextAlign.Center
+        )
+    }
+}
+
+@Composable
+fun Table2ColumnDetailRow(
+    firstColumnWeight: Float = .7f,
+    secondColumnWeight: Float = .3f,
+    isFirstColumnValueMatched: Boolean, // ถ้าไม่ใช้ ค่อยลบออก
+    firstColumnValue: String,
+    secondColumnValue: String,
+    rowBottomSpace: Dp = 10.dp
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(IntrinsicSize.Min)
+    ) {
+        Text(
+            modifier = Modifier.weight(firstColumnWeight),
+            text = firstColumnValue,
+            style = FontStyles.txt16,
+            fontWeight = FontWeight.Medium,
+            color = if (isFirstColumnValueMatched) BaseGreen else Color.Black
+        )
+        Spacer(modifier = Modifier.width(10.dp))
+        Text(
+            modifier = Modifier
+                .background(White87)
+                .padding(horizontal = 4.dp)
+                .fillMaxHeight()
+                .weight(secondColumnWeight),
+            text = secondColumnValue,
+            style = FontStyles.txt16,
+            fontWeight = FontWeight.Bold,
+            color = StatusRed,
+            textAlign = TextAlign.Center
+        )
+    }
+    Spacer(modifier = Modifier.height(rowBottomSpace))
+}
+
+@Preview(showBackground = true, locale = "th")
+@Composable
+fun BaseTablePreview() {
+    Column {
+        // 2 Column Table
+        Table2ColumnHeaderRow()
+        Table2ColumnDetailRow(
+            isFirstColumnValueMatched = false,
+            firstColumnValue = "AAAAAAAA18",
+            secondColumnValue = ""
+        )
+        Table2ColumnDetailRow(
+            isFirstColumnValueMatched = true,
+            firstColumnValue = "AAAAAAAA17",
+            secondColumnValue = "YES"
+        )
+        Table2ColumnDetailRow(
+            isFirstColumnValueMatched = false,
+            firstColumnValue = "AAAAAAAA12",
+            secondColumnValue = ""
+        )
+
+        // 3 Column Table
+        Table3ColumnHeaderRow()
+        Table3ColumnDetailRow(
+            isFirstColumnValueMatched = false,
+            firstColumnValue = "AAAAAAAA18",
+            secondColumnValue = "",
+            thirdColumnValue = "16W",
+        )
+        Table3ColumnDetailRow(
+            isFirstColumnValueMatched = true,
+            firstColumnValue = "AAAAAAAA17",
+            secondColumnValue = "YES",
+            thirdColumnValue = "16W",
+        )
+        Table3ColumnDetailRow(
+            isFirstColumnValueMatched = false,
+            firstColumnValue = "AAAAAAAA12",
+            secondColumnValue = "",
+            thirdColumnValue = "16W",
+        )
+    }
+}
