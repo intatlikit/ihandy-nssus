5d52b5bab651895f5672e6f93242bfdba9973307
Intat Likitpolchaloon <intat@orcsoft.co.th>
2024-11-06 14:07:52 +0700

    create common menu list and add into each user role menu list

--- app/src/main/java/com/nssus/ihandy/data/constant/HomeMenuConstant.kt
+++ app/src/main/java/com/nssus/ihandy/data/constant/HomeMenuConstant.kt
@@ -5,18 +5,21 @@ import com.nssus.ihandy.model.home.DisplayHomeModel
 import com.nssus.ihandy.model.home.HomeMenuItem
 
 object HomeMenuConstant {
+    private val commonMenuLs = listOf(
+        HomeMenuItem.InventoryTakingScreen,
+        HomeMenuItem.YardEntryScreen
+    )
+
     val PACKING_OP_MENU = DisplayHomeModel(
         titleId = R.string.home_user_role_packing_title,
-        menuLs = listOf(
-            HomeMenuItem.InventoryTakingScreen,
+        menuLs = commonMenuLs + listOf(
             HomeMenuItem.YardEntryScreen
         )
     )
 
     val SHIPPING_OP_MENU = DisplayHomeModel(
         titleId = R.string.home_user_role_shipping_title,
-        menuLs = listOf(
-            HomeMenuItem.YardEntryScreen,
+        menuLs = commonMenuLs + listOf(
             HomeMenuItem.InventoryTakingScreen
         )
     )
