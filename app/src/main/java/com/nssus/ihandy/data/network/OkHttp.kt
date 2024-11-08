00b3219ec342c089ce5773141b3115f7f108995f
Intat Likitpolchaloon <intat@orcsoft.co.th>
2024-11-08 14:00:55 +0700

    create provideOkHttpClientForInterceptor and add authInterceptor into provideOkHttpClient

--- app/src/main/java/com/nssus/ihandy/data/network/OkHttp.kt
+++ app/src/main/java/com/nssus/ihandy/data/network/OkHttp.kt
@@ -3,6 +3,7 @@ package com.nssus.ihandy.data.network
 import com.chuckerteam.chucker.api.ChuckerInterceptor
 import com.nssus.ihandy.BuildConfig
 import com.nssus.ihandy.data.constant.ValueConstant
+import com.nssus.ihandy.data.interceptor.AuthInterceptor
 import com.nssus.ihandy.data.interceptor.NoConnectionInterceptor
 import com.nssus.ihandy.data.interceptor.RequestInterceptor
 import okhttp3.OkHttpClient
@@ -24,14 +25,30 @@ fun provideOkHttpClient(
     chucker: ChuckerInterceptor,
     noConnectionInterceptor: NoConnectionInterceptor,
     requestInterceptor: RequestInterceptor,
-//    authInterceptor: AuthInterceptor
+    authInterceptor: AuthInterceptor
 ): OkHttpClient {
     return OkHttpClient().newBuilder().apply {
         addNetworkInterceptor(loggingInterceptor)
         addInterceptor(chucker)
         addInterceptor(noConnectionInterceptor)
         addInterceptor(requestInterceptor)
-//        addInterceptor(authInterceptor)
+        addInterceptor(authInterceptor)
+        connectTimeout(ValueConstant.TIME_OUT, TimeUnit.SECONDS)
+        readTimeout(ValueConstant.TIME_OUT, TimeUnit.SECONDS)
+        writeTimeout(ValueConstant.TIME_OUT, TimeUnit.SECONDS)
+        retryOnConnectionFailure(true)
+    }.build()
+}
+
+fun provideOkHttpClientForInterceptor(
+    loggingInterceptor: HttpLoggingInterceptor,
+    chucker: ChuckerInterceptor,
+    noConnectionInterceptor: NoConnectionInterceptor
+): OkHttpClient {
+    return OkHttpClient().newBuilder().apply {
+        addNetworkInterceptor(loggingInterceptor)
+        addNetworkInterceptor(chucker)
+        addInterceptor(noConnectionInterceptor)
         connectTimeout(ValueConstant.TIME_OUT, TimeUnit.SECONDS)
         readTimeout(ValueConstant.TIME_OUT, TimeUnit.SECONDS)
         writeTimeout(ValueConstant.TIME_OUT, TimeUnit.SECONDS)
