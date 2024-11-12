fc2d2d984838660fd44a36fb3a02b2f4e16d0a76
Intat Likitpolchaloon <intat@orcsoft.co.th>
2024-11-12 16:27:27 +0700

    add int extension for set padstart format string

--- app/src/main/java/com/nssus/ihandy/data/extension/IntExt.kt
+++ app/src/main/java/com/nssus/ihandy/data/extension/IntExt.kt
@@ -1,1 +1,5 @@
 package com.nssus.ihandy.data.extension
+
+fun Int.padStart(): String = String.format("%02d", this)
+
+fun Int.padStartCustom(resultDigitLength: Int = 2, padChar: String = "0"): String = toString().padStart(resultDigitLength, padChar.first())
