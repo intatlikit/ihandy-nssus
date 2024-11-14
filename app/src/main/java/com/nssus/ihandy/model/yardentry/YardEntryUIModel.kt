cbb9a4c74ffff88917662068c4002a43a8138653
Intat Likitpolchaloon <intat@orcsoft.co.th>
2024-11-14 17:10:46 +0700

    add variable to check tick select in remove coil detail screen

--- app/src/main/java/com/nssus/ihandy/model/yardentry/YardEntryUIModel.kt
+++ app/src/main/java/com/nssus/ihandy/model/yardentry/YardEntryUIModel.kt
@@ -2,6 +2,7 @@ package com.nssus.ihandy.model.yardentry
 
 data class CoilDetailItem(
     var isMatched: Boolean = false,
+    var isSelectedRemove: Boolean = false,
     // ใช้โมเดลเอพีไอนั้น
     var coilNo: String = "",
     var status: String = "",
