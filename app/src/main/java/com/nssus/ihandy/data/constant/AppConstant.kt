02bc8775e00d6ccd608ba015f9a4117ef4f98dfe
Intat Likitpolchaloon <intat@orcsoft.co.th>
2024-11-29 13:57:38 +0700

    convert variable type from string to string state

--- app/src/main/java/com/nssus/ihandy/data/constant/AppConstant.kt
+++ app/src/main/java/com/nssus/ihandy/data/constant/AppConstant.kt
@@ -1,9 +1,14 @@
 package com.nssus.ihandy.data.constant
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+
 object AppConstant {
 
     var NETWORK_IP: String? = null
-    var USER_ROLE = ""
+//    var USER_ROLE = ""
+    var USER_ROLE by mutableStateOf("")
     var USERNAME = ""
     var APP_TOKEN = ""
 
