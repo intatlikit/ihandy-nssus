5e66c79d40aaead7a5a8edc95ab3e30d990c6d5c
Intat Likitpolchaloon <intat@orcsoft.co.th>
2024-12-06 13:08:14 +0700

    add main menu global state variable

--- app/src/main/java/com/nssus/ihandy/data/constant/AppConstant.kt
+++ app/src/main/java/com/nssus/ihandy/data/constant/AppConstant.kt
@@ -3,6 +3,7 @@ package com.nssus.ihandy.data.constant
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import com.nssus.ihandy.model.home.DisplayHomeModel
 
 object AppConstant {
 
@@ -11,5 +12,6 @@ object AppConstant {
     var USER_ROLE by mutableStateOf("")
     var USERNAME = ""
     var APP_TOKEN = ""
+    var MAIN_MENU by mutableStateOf(DisplayHomeModel())
 
 }
